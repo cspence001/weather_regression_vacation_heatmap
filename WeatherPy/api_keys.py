756e176e9f40172ca53514aba47978a5780eb770
Ciara Spencer <ciara.spencer314@gmail.com>
2020-12-31 16:36:38 -0500

    updating notebooks

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "375862b4fbd3dc466467cd46984ea787"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyB3EPwOR6dbpj3ymE1TxtuOq4x-gsY8PMU"
