8e53283acf45a3e14d0b1fda8ae001938b32ab46
Ciara Spencer <ciara.spencer314@gmail.com>
2021-01-07 14:41:20 -0500

    pushing weatherpy copy, with exported csv

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "375862b4fbd3dc466467cd46984ea787"
+weather_api_key = "65e67b99df7e4d1a7b6d4e19ea6783d8"
 
 # Google API Key
 g_key = "AIzaSyB3EPwOR6dbpj3ymE1TxtuOq4x-gsY8PMU"
